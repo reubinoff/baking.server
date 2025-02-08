821f89053c28cc8ff5b93341aefcee0163880ee3
moshe reubinoff <reubinoff@gmail.com>
2025-02-08 22:07:50 +0200

    Refactor configuration classes for improved structure and caching; add SQLModel import for Alembic scripts

--- server/config.py
+++ server/config.py
@@ -1,21 +1,32 @@
-from pydantic_settings import BaseSettings
+from functools import cache
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class BakingConfig(BaseSettings):
+class BaseBakingConfig(BaseSettings):
+    model_config = SettingsConfigDict(frozen=True)
+
+
+class DBSettings(BaseBakingConfig):
+    db_hostname: str
+    db_hostname: str
+    db_password: str
+    db_username: str
+    db_name: str
+    db_port: int = 5432
+    db_enforce_ssl_mode: bool = True
+
+
+class BakingConfig(BaseBakingConfig, DBSettings):
     is_debug: bool = False
     service_name: str = "testing-service"  # will be replaced in env var
 
     log_level: str = "DEBUG"
 
-    # db_conn_str: str
-    # db_name: str
-
-    db_debug_drop_in_startup: bool = False
-
-    root_path: str = ""
-
     # azure_storage_connection_string: str
     azure_cdn_storage_base_url: str = "https://images.baking.reubinoff.com"
 
 
-settings = BakingConfig()
+@cache
+def get_config() -> BakingConfig:
+    return BakingConfig()
